c5330a6619de85a66024798374d1de7982511dde
Sebastien Gillet <sbsgillet@gmail.com>
2015-12-28 19:07:57 +0100

    temp commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cineAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cineAngularApp
+ */
+angular.module('cineAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
